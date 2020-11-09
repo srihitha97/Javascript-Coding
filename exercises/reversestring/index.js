784c12b8f5f5f567dc801da2325b72bf6e4c226c
Srihitha <36701923+srihitha97@users.noreply.github.com>
2020-11-09 21:24:36 +0530

    Add files via upload

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  var result = ""
+    for (var i = str.length -1; i >= 0; i--) {
+        result = result + str[i]
+    }
+    return result
+}
+
+module.exports = reverse;
